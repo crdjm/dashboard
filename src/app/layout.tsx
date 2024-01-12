ac6beed38d9fc2061bbc69ce6d453c1f5f212170
crdjm <djm@logictran.com>
2024-01-12 10:55:36 -0600

    Sidebar updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 
+import SideNavbar from "@/components/SideNavbar"
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen w-full bg-white text-black flex", inter.className, { "debug-screens": process.env.NODE_ENV === 'development' })}>
-        <p className="border">Sidebar</p>
+        {/* <p className="border">Sidebar</p> */}
+        <SideNavbar />
         <div className="p-8 w-full">
           {children}
         </div>
